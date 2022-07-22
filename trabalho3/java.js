62ed6396da69d953b302969aa9bca6b0c4784bd3
Allan182 <allan.furlani@hotmail.com>
2022-07-22 16:46:56 -0300

    Tabela Adicionada ao Site

--- trabalho3/java.js
+++ trabalho3/java.js
@@ -1,33 +1,20 @@
 var inData = document.getElementById("inData")
 var btProcurar = document.getElementById("btProcurar")
-var outSaida = document.getElementById("autSaida");
-var saida2 = true;
+var autSaida = document.getElementById("autSaida");
+
 function relatorio() {
-    var data = Number(inData.value);
-    var saida = "No dia " + data + "/08/2022" + " á estas horas marcados:" + "\n"
-
-    if (inData.value == "" || data <= 0 || data > 20 || data != Math.floor(data)) {
-        alert("Os Dados Inseridos no Campo Dia Estão Invalidos!!!")
-    } else {
-
-        for (var aux = 0; aux < vetDia.length; aux++) {
-            switch (vetServico[aux]) {
-                case "Lavagem interna":
-                case "Lavagem externa com polimento":
-                case "Lavagem externa":
-                case "Lavagem completa com polimento":
-                case "Lavagem completa":
-                    if (vetDia[aux] == data) {
-                        saida += "\n" + "As " + vetHora[aux] + " tem uma " + vetServico[aux] + "\n";
-                    }
-                    break
-                default:
-                    saida = "No dia " + data + "/08/2022" + " á estas horas marcados: Não tem"
-            }
-
-
-            outSaida.textContent = saida;
+    var data = inData.value
+    var saida = "No dia " + data + "/08/2022" + " á estas horas marcados: "
+    var aux = 0
+    while (aux < vetDia.length) {
+        if (vetDia[aux] == data) {
+                
+                saida+= "Ás " + vetHora[aux] + " tem uma " + vetServico[aux] + "\n" ; 
+                aux++
+        } else {
+            aux++;
         }
     }
+ autSaida.textContent = saida
 }
 btProcurar.addEventListener("click", relatorio)
